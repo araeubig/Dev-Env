69a599b997dd8c2b82e8ee055b36cd97915df2eb
araeubig <amr@bild-werk-statt.ch>
2023-12-26 13:44:12 +0000

    Update from https://github.com/araeubig/vip-codespaces/commit/c263fe09a5824ca8ecea8bd0b8f1ac1a5a3b1aaf

--- .devcontainer/features/wordpress/010-wplogin.tpl
+++
@@ -1,7 +0,0 @@
-# shellcheck shell=bash
-
-echo "*** WordPress Credentials ***"
-echo "Login URL: ${WP_URL}/wp-login.php"
-echo "Username: admin"
-echo "Password: password"
-echo
--- .devcontainer/features/wordpress/wordpress-post-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -f composer.json ] && [ -x /usr/local/bin/composer ]; then
-    /usr/local/bin/composer install -n || true
-fi
-
-if [ ! -f "${HOME}/.local/share/wp-codespaces/login/010-wplogin.sh" ]; then
-    /usr/local/bin/setup-wordpress.sh
-fi
